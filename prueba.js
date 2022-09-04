ba69c4847472ef6d690f38d7978e80edde2418cf
Davidestroy <christian-811@hotmail.com>
2022-09-04 15:17:26 -0500

    Eliminación archivos innecesarios

--- prueba.js
+++
@@ -1,7 +0,0 @@
-function funcion1(){
-
-    let x = parseInt(prompt("Digite numero 1 "));
-    let y = parseInt(prompt("Digite numero 2 "));
-    let z = parseInt((x+y));
-    document.getElementById("prueba").innerHTML = "el valor de z es "+z;
-  }
